9be298f0ce9cea6e33937f7b33722f327453cc4b
kalharaweragoda <kalharaweragoda@gmail.com>
2026-07-11 06:11:44 +0530

    @Nullable Bundle

--- app/src/main/java/lk/jiat/activity/activity/HomeActivity.java
+++ app/src/main/java/lk/jiat/activity/activity/HomeActivity.java
@@ -3,6 +3,7 @@ package lk.jiat.activity.activity;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -17,14 +18,10 @@ public class HomeActivity extends AppCompatActivity {
     }
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         //setContentView(R.layout.activity_home);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+
     }
 }
