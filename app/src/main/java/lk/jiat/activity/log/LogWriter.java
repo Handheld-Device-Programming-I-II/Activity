ae4deaec15fe6fcb3a181703b5b14e7cc369e088
kalharaweragoda <kalharaweragoda@gmail.com>
2026-07-12 06:08:17 +0530

    public static void writerInfoLog

--- app/src/main/java/lk/jiat/activity/log/LogWriter.java
+++ app/src/main/java/lk/jiat/activity/log/LogWriter.java
@@ -1,4 +1,11 @@
 package lk.jiat.activity.log;
 
+import android.content.Context;
+import android.util.Log;
+
 public class LogWriter {
+
+    public static void writerInfoLog(Context, String message){
+        Log.i(context, );
+    }
 }
